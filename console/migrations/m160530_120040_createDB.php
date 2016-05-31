32c037caaa67f09751a12f836de78d51bcbf58ac
reinierdlp <reinier@releaz.nl>
2016-05-31 10:15:21 +0200

    create database 0.1

--- console/migrations/m160530_120040_createDB.php
+++ console/migrations/m160530_120040_createDB.php
@@ -37,29 +37,45 @@ class m160530_120040_createDB extends Migration
         //create table
         $this->createTable('action',ArrayHelper::merge([
             'id' => $this->primaryKey(11),
+            'organization_id' => $this->integer(11),
             'name' => $this->string(128)->notNull(),
-            'intro' => $this->binary(2000),
-            'description' => $this->binary(),
-            'image' => $this->string(128),
-            'image_facebook' => $this->string(128),
-            'description_facebook' => $this->binary(2000),
-            'image_twitter' => $this->texz(128),
-            'description_twitter' => $this->text(140),
+            'intro' => $this->binary(2000)->notNull(),
+            'description' => $this->binary()->notNull(),
+            'image' => $this->string(128)->notNull(),
+            'image_facebook' => $this->string(128)->notNull(),
+            'description_facebook' => $this->text(63206)->notNull(),
+            'image_twitter' => $this->text(128)->notNull(),
+            'description_twitter' => $this->text(140)->notNull(),
+            'begin' => $this->dateTime()->notNull(),
+            'end' => $this->dateTime()->notNull(),
+            'url' => $this->string(255)->notNull(),
 
         ],$this->defaultFields));
 
         $this->createTable('actionFields',ArrayHelper::merge([
             'id' => $this->primaryKey(11),
-            'label' => $this->string(128),
-            'required' => $this->boolean()->defaultValue(false)->notNull()
+            'label' => $this->string(128)->notNull(),
+            'required' => $this->boolean()->defaultValue(false)->notNull(),
+            'type' => $this->integer(1)->defaultValue(0)->notNull(),
+            'action_id' => $this->integer(11),
         ],$this->defaultFields));
+
+        //set relations
+        $this->addForeignKey('action_organization_fk','action','organization_id','organization','id','CASCADE','NO ACTION');
+        $this->addForeignKey('actionFields_action_fk','actionFields','action_id','action','id','CASCADE','NO ACTION');
 
         }
 
     public function down()
     {
-        $this->dropTable('organization');
+
+        //drop relations
+        $this->dropForeignKey('action_organization_fk','action');
+        $this->dropForeignKey('actionFields_action_fk','actionFields');
+        //drop tables
+        $this->dropTable('actionFields');
         $this->dropTable('action');
+        $this->dropTable('organization');
         return true;
     }
 
