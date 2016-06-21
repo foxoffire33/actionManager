841728e71caec674b868b04ef6101a273ff2cdea
reinierdlp <reinier@releaz.nl>
2016-06-21 14:55:04 +0200

    nginx config

--- api/config/main-local.php
+++ api/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'brahythjZxBM-G27bIFkLZDiD4Oz1kNc',
+            'cookieValidationKey' => 'kTbEte92Jc9RNLbuzl6W8NvY6gCteVWt',
         ],
     ],
 ];
